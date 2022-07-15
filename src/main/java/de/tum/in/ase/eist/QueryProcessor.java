8dd7a9e20296bcb7d72da68a3b8db45888774d98
Maximilian von Boxberg <ge42ket@mytum.de>
2022-07-15 13:22:22 +0200

    1

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,28 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Super-Tutorium";
-        } else { // TODO extend the programm here
+        }
+        else if(query.contains("plus")){
+            String[] splitArr = query.split(":");
+
+            char[] chars = splitArr[1].toCharArray();
+
+            int result = 0;
+
+            String[] splitArr2 = splitArr[1].split(" ");
+
+            for(String element: splitArr2){
+                try {
+                    int val = Integer.parseInt(element);
+                    result += val;
+                } catch (NumberFormatException e) {
+                    System.out.println("Input String cannot be parsed to Integer.");
+                }
+            }
+
+            return String.valueOf(result);
+        }
+        else { // TODO extend the programm here
             return "";
         }
     }
